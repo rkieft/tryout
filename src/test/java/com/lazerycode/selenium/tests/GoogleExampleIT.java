bc36cc7d380823cc1653a0a98a6896c38b484059
Richard Kieft <Richard.Kieft@rabobank.nl>
2017-11-23 15:03:40 +0100

    tryout from other github project to set it to use selenium grid

--- src/test/java/com/lazerycode/selenium/tests/GoogleExampleIT.java
+++ src/test/java/com/lazerycode/selenium/tests/GoogleExampleIT.java
@@ -1,11 +1,8 @@
 package com.lazerycode.selenium.tests;
 
 import com.lazerycode.selenium.DriverBase;
-
 import com.lazerycode.selenium.page_objects.GoogleHomePage;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
